e237f27b6eb30f9547e223ece91006b6edb613f6
Henry Steinhauer <henry.steinhauer@i22.de>
2021-04-12 20:06:04 +0200

    feat: add sass package

---
+++ components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="header__fill-box"/>
+    <h1>Henry Steinhauer</h1>
+    <div class="header__fill-box"/>
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator'
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  &__fill-box {
+    display: block;
+    width: 40%;
+    height: 40vh;
+    background-color: black;
+  }
+}
+</style>
