4c2d936b03baeccc681d4346d62e57187819ff0e
Henry Steinhauer <henry.steinhauer@i22.de>
2021-05-23 14:37:49 +0200

    feat: Add hover effect on social-bar

--- components/ui/social-media-bar.vue
+++ components/ui/social-media-bar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="social-media-bar">
     <template v-for="(icon, index) in Icons">
-      <a :key="index" :href="icon.src" class="social-media-bar__icon">{{ icon.label }}</a>
+      <a :key="index" :href="icon.src" class="social-media-bar__icon-container">
+        <span class="social-media-bar__icon">{{ icon.label }}</span>
+      </a>
     </template>
   </div>
 </template>
@@ -31,9 +33,22 @@ export default class SocialMediaBar extends Vue {
     color: black;
     text-decoration: none;
     font-size: 1.5vw;
+    position: absolute;
 
-    &:hover {
-      background-image: url('./assets/illustration-circle-contact.svg');
+    &-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      width: 100%;
+      height: 16vh;
+
+      &:hover {
+        background-image: url('./assets/illustration-circle-contact.svg');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+      }
     }
   }
 }
