382f128fc0a2d3b0f6c814adb4737053ae23635a
Henry Steinhauer <henry.steinhauer@i22.de>
2021-05-23 11:36:18 +0200

    feat: Create about-me component

---
+++ components/ui/about-me.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about-me">
+    <h3 class="about-me__headline">{{ aboutMeInfo.headline }}</h3>
+    <p class="about-me__desc">{{ aboutMeInfo.description }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+export interface AboutMeInterface {
+  headline: string,
+  description: string
+}
+
+@Component
+export default class AboutMe extends Vue {
+  @Prop({ required: true })
+  readonly aboutMeInfo!: AboutMeInterface
+}
+</script>
+
+<style scoped lang="scss">
+.about-me {
+  &__headline {
+    float: left;
+    margin: 3px 30px 30px 0;
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  &__desc {
+    font-size: 21px;
+  }
+}
+</style>
