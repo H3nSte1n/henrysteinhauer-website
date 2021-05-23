7c065f4026e6e5b928662a4d1e908a27519ec791
Henry Steinhauer <henry.steinhauer@i22.de>
2021-05-23 11:33:36 +0200

    feat: Create navigation and header

---
+++ components/layouts/header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="header">
+    <div class="header-row">
+      <h2 class="header-row--big">Full Stack</h2>
+      <p class="header-row--small"></p>
+    </div>
+    <div class="header-row">
+      <h1 class="header-row--small header-row--small-invert">Henry<br>Steinhauer</h1>
+      <h2 class="header-row--big">Developer</h2>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  margin-bottom: 100px;
+
+  @media screen and (min-width: 1480px) {
+    margin-bottom: 200px;
+  }
+
+  &-row {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    margin: 30px 0;
+
+    &--big {
+      font-size: 15vw;
+      font-weight: 400;
+    }
+
+    &--small {
+      font-size: 26px;
+      font-weight: 400;
+
+      &-invert {
+        padding-left: 26px;
+
+        @media screen and (min-width: 640px) {
+          padding-left: 6vw;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ components/layouts/navigation.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="nav__container">
+    <div :class="{'nav': true, 'nav--active': toggleActive}">
+      <a href="#header">Henry Steinhauer</a>
+      <nav>
+        <ul @click="toggleMenue" :class="{'nav__list': true, 'nav__list--active': toggleActive}">
+          <li v-for="(nav, index) in navs"
+            :key="index"
+            class="nav__list-item"
+            :style="{top: index > 0 && toggleActive ? 'calc(50% - 20%)' : '0px'}"
+          >
+            <a :href="nav.link">
+              {{nav.name}}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+interface NavigationInterface {
+  name: String,
+  link: String
+}
+
+@Component
+export default class Navigation extends Vue {
+  readonly navs: Array<NavigationInterface> = [
+    {
+      name: "MENU",
+      link: "#highlights"
+    },
+    {
+      name: "Highlights",
+      link: "#highlights"
+    },
+    {
+      name: "Skills",
+      link: "#skills"
+    },
+    {
+      name: "Social Media",
+      link: "#social"
+    },
+    {
+      name: "contact",
+      link: "#contact"
+    },
+  ]
+
+  toggleActive: Boolean = false;
+
+  toggleMenue() {
+    if(window.innerWidth <= 700) {
+      this.toggleActive = !this.toggleActive;
+    } else {
+      this.toggleActive = false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  overflow: hidden;
+  position: relative;
+
+  &--active {
+    height: 100vh;
+  }
+
+  &__container {
+    width: 100%;
+    position: sticky;
+    top: 0px;
+    padding: 15px 20px 0 15px;
+    background-color: white;
+    z-index: 1;
+  }
+
+  &__list {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: 0px;
+
+    &--active {
+      height: 100%;
+    }
+
+    @media screen and (min-width: 700px) {
+      flex-direction: row;
+      height: 100%;
+    }
+
+    &-item {
+      padding: 0 0 20px 0;
+      text-align: right;
+      position: relative;
+
+      @media screen and (min-width: 700px) {
+        padding: 0 15px;
+      }
+
+      &:last-child {
+        @media screen and (min-width: 700px) {
+          padding: 0 0 0 15px;
+        }
+      }
+      &:first-child {
+        top: 0px;
+        padding-bottom: 33px;
+        @media screen and (min-width: 700px) {
+          display: none;
+          padding-bottom: 33px;
+        }
+      }
+    }
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    text-transform: uppercase;
+  }
+}
+</style>
