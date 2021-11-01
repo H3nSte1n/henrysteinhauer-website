d55af43114671066fb91143d9c0f0a412d2ecdd0
Henry Steinhauer <henry.steinhauer@i22.de>
2021-11-01 14:15:59 +0100

    feat: Update observer mixin

--- mixins/number-increase-animation.ts
+++ mixins/number-increase-animation.ts
@@ -2,7 +2,7 @@ import { Vue, Component } from 'vue-property-decorator';
 
 export interface MethodObjInterface {
   name: string;
-  params: Array<number | string>;
+  params: Array<number | string | Array<string | number>>;
 }
 
 export interface AnimationInterface {
@@ -27,6 +27,27 @@ export class Animation extends Vue {
     const htmlElement = this.$refs[element] as HTMLElement;
     if (htmlElement.classList.contains(`${element}--visible`)) return;
     htmlElement.classList.add(`${element}--visible`);
+  }
+
+  drawSVG(totalLength: number, element: string) {
+    window.requestAnimationFrame(() => {
+      const path = this.$refs[element] as HTMLElement;
+      const scrollPercentage = this.getScrollPercentage(0.2, path);
+      const drawLength = totalLength * scrollPercentage;
+      if (drawLength < totalLength && drawLength > 0) path.style.strokeDashoffset = `${drawLength}`;
+    });
+  }
+
+  getScrollPercentage(gapBottom: number, path: HTMLElement) {
+    const boundaries = path.getBoundingClientRect();
+    return boundaries.y / window.innerHeight + gapBottom;
+  }
+
+  animateOnScroll(method: string, params: Array<number | string>) {
+    console.log(method, params);
+    document.addEventListener('scroll', () => {
+      this[method as keyof Animation](...params);
+    });
   }
 
   private createObserver(animation: AnimationInterface) {
